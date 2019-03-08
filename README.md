2e8177d3da251f7a90b1addd26e4fda51a7e3c4d
浩哥 <wx_86a60aa624564c0ea4576b8c80e28fcc@git.code.tencent.com>
2019-03-08 11:25:12 +0800

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# 开发环境
+JDK 10+
+Maven 3.5+
+
+因本项目使用了var语法，故需要安装JDK10+的环境才可编译运行；<br/>
+另使用lombok组件，故需要安装lombok Plugin才能正常显示Get/Set方法；
+
+# 项目配置
+请clone项目后，使用idea打开项目目录即可自动加载；<br/>
+
+# 项目调试
+选择WebApplication文件，右键运行Main方法；
---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Map TestAction() {
+        var map = new HashMap<>();
+        map.put("state", "ok");
+        return map;
+    }
+}
